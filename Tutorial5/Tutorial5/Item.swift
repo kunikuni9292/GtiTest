d2b18df7bebb54ba378eb96585cfa09bb85f2b25
くにくに <kunitatsu427@gmail.com>
2021-07-10 11:27:49 +0900

    SwiftでAPI読み込みのサンプル作成

---
+++ Tutorial5/Tutorial5/Item.swift
@@ -0,0 +1,30 @@
+//
+//  Item.swift
+//  Tutorial5
+//
+//  Created by 古堅ニノスカ on 2020/11/28.
+//
+
+import UIKit
+
+// プロパティ
+class Item {
+   private var name: String!
+   private var atack: String!
+    
+    // コンストラクタ
+    init(name: String, atack: String) {
+        self.name = name
+        self.atack = atack
+    }
+    
+    func getname() -> String {
+        return name
+    }
+    
+    
+    func getatack() -> String {
+        return atack
+    }
+    
+}
